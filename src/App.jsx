00e568f0110019000ad51a639e2c5a3a54bf8bdc
Aonggggggggggs <109132779+Aonggggggggggs@users.noreply.github.com>
2023-01-16 17:09:03 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,68 @@
-import * as React from "react "
-const welcome ={
-  greeting: "Hey",
-  title: "React",
-}
-function App() {
-  
+import * as react from 'react';
+
+const App = () => {
+  const storise =
+  [
+    {
+      title: 'react',
+      url: 'https://readux.js.org',
+      author: ('Dan Abramov', 'Andrew Clak'),
+      num_comments: 2,
+      point: 5,
+      subjectid: 1
+    },
+
+    {
+      title: 'readux',
+      url: 'https://readux.js.org',
+      author: 'Jordan walk',
+      num_comments: 2,
+      point: 5,
+      subjectid: 1
+    }
+];
   return (
     <div>
-      <div>
-        <h1>{welcome.greeting} {welcome.title}</h1>
-        <label htmlFor="search">Search: </label>
-         <input type="text" id= "search" />
-      </div>
-      
+      <h1> HAcker Story</h1>
+      <Search />
+      <hr />
+      <List list={storise} />
+    </div>
+  )
+
+}
+
+const Search = () => {
+  const handleChange = (event) => {
+    console.log(event)
+    console.log(event.target.value);
+  }
+
+  return (
+    <div>
+      <label htmlFor="search">search </label>
+      <input type="text" id='search' onChange={handleChange} />
     </div>
   )
 }
 
+
+const List = (props) => (
+  <ul>
+    {props.list.map(
+      (item) => <Item key={item.objectID} item={item} />)
+    }
+  </ul>
+)
+const Item = (props) => (
+  <li>
+    <span>
+      <a href={props.item.url}>{props.item.title} </a>
+    </span>
+    <span>{props.item.author}</span>
+    <span>{props.item.num_comments}</span>
+    <span>{props.item.point}</span>
+  </li>
+
+)
 export default App
